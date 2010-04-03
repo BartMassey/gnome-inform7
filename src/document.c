4fcd202555afd18608b4502670ce7fef4f6235ce
Philip Chimento <philip.chimento@gmail.com>
2010-04-03 14:46:29 +0000

    Fixed uncomment accelerator
    git-svn-id: https://gnome-inform7.svn.sourceforge.net/svnroot/gnome-inform7/branches/gtk2-12-branch@177 7a7d6849-5d27-4570-9ef9-9f38b82aaeff

--- src/document.c
+++ src/document.c
@@ -145,7 +145,7 @@ i7_document_init(I7Document *self)
 	const gchar *selection_actions[] = {
 		"scroll_selection", "<ctrl>J",
 		"comment_out_selection", "<ctrl>slash",
-		"uncomment_selection", "<ctrl><shift>slash", /* FIXME doesn't work?! */
+		"uncomment_selection", "<ctrl>question", /* Ctrl-Shift-/ doesn't work, Gnome bug #614146 */
 		NULL
 	};
 	const gchar *copy_actions[] = {
