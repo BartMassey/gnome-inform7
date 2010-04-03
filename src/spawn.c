da38108814c6c9013035e4c3018a8a6299f5b838
Philip Chimento <philip.chimento@gmail.com>
2010-04-03 14:21:59 +0000

    Echo command invocation to stdout
    git-svn-id: https://gnome-inform7.svn.sourceforge.net/svnroot/gnome-inform7/branches/gtk2-12-branch@174 7a7d6849-5d27-4570-9ef9-9f38b82aaeff

--- src/spawn.c
+++ src/spawn.c
@@ -119,7 +119,16 @@ run_command(const gchar *wd, gchar **argv, GtkTextBuffer *output)
     GError *err = NULL;
     GPid child_pid;
     gint stdout_fd, stderr_fd;
-    
+
+	/* Echo the invocation to the output buffer */
+	gchar *args = g_strjoinv(" ", argv + 1);
+	GtkTextIter end;
+	gtk_text_buffer_get_end_iter(output, &end);
+	gchar *invocation = g_strdup_printf("\n%s \\\n\t%s\n", argv[0], args);
+	gtk_text_buffer_insert(output, &end, invocation, -1);
+	g_free(args);
+	g_free(invocation);
+	
     if (!g_spawn_async_with_pipes(
       wd,           /* working directory */
       argv,         /* command and arguments */
@@ -177,6 +186,15 @@ run_command_hook(const gchar *wd, gchar **argv, GtkTextBuffer *output,
     GError *err = NULL;
     GPid child_pid;
     gint stdout_fd, stderr_fd;
+
+	/* Echo the invocation to the output buffer */
+	gchar *args = g_strjoinv(" ", argv + 1);
+	GtkTextIter end;
+	gtk_text_buffer_get_end_iter(output, &end);
+	gchar *invocation = g_strdup_printf("\n%s \\\n\t%s\n", argv[0], args);
+	gtk_text_buffer_insert(output, &end, invocation, -1);
+	g_free(args);
+	g_free(invocation);
     
     if (!g_spawn_async_with_pipes(
       wd,           /* working directory */
